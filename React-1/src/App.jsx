8a38246b7d190d095b79706a1eacd01e7782efa0
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-03 23:26:52 +0530

    some react code added

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
       <Products/>
       <footer>
         <p>�� 2022 My Company. All rights reserved.</p>
+        <p>Terms of Service | Privacy Policy</p>
+        <p>Contact: 123-456-7890</p>
+        <p>�� My Company, Inc.</p>
+        <p>123 Main St, Anytown, USA 12345</p>
       </footer>
     </>
   )
