f31e889aa5064c5deeed583e2f773f18198683df
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-03 23:15:06 +0530

    some react code added

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+// import DataFetch from "./components/DataFetch";
+
+function App() {
+  return (
+    <>
+      {/* <DataFetch /> */}
+      <h1>Rahul Kumar</h1>
+      <div>
+        <video width="800px" src="https://9fd5aba8692d70cefef4b3581c1daecf.cdn.bubble.io/f1724490216027x706658625622187400/hero%20section%20web%20%282%29.mp4?_gl=1*1rk115d*_gcl_au*MTMzOTk3MDA3OC4xNzE5MjMxMjA4LjE0ODk3ODM1NTMuMTcyMzg0ODk5MS4xNzIzODUwMjk2*_ga*MTU1NDIzMTQ1LjE3MTEyMzQ3NDY.*_ga_BFPVR2DEE2*MTcyNDQ4MzkwNi4xMDQuMS4xNzI0NDkwMTM5LjQ2LjAuMA..">
+        </video>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
