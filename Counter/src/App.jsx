13f395cc87c4a8b52cdd69d9c400550e7bd3d832
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-12 20:06:09 +0530

    some part done of counter

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,24 +1,16 @@
-import './App.css'
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [count,setCount] = useState(0)
-
-  const decrement = () =>{
-    setCount(count-1)
-  }
-  const increment = () =>{
-    setCount(count+1)
-  }
-
   return (
     <>
-       <h1>Counter</h1>
-        <button onClick={()=>decrement()}>decrement</button>
-       <h2>Count:{count}</h2>
-       <button onClick={()=>increment()}>Increment</button>
+      <h1>Counter</h1>
+      <button></button>
+      <p>{count}</p>
+      <button></button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
