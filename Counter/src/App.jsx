f52e4e506ed7d3053e6dd4fb5349018becd857f7
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-12 20:09:16 +0530

    counter done

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 
 function App() {
   const [count,setCount] = useState(0)
+  const decrement = () => {
+    setCount(count-1)
+  }
+  const increment = () =>{
+    setCount(count+1)
+  }
   return (
     <>
       <h1>Counter</h1>
-      <button></button>
+      <button onClick={decrement}>-</button>
       <p>{count}</p>
-      <button></button>
+      <button onClick={increment}>+</button>
     </>
   );
 }
