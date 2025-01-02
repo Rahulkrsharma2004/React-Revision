06c986bed0eb0371367204ceb9996124143860ff
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-01-02 23:39:10 +0530

    some changes

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,7 +1,17 @@
 export default function App() {
+
+
+  const handleLogin = () =>{
+    
+  }
+
   return (
     <>
       <h1>WELCOME TO HOME PAGE</h1>
+      <div style={{display:"flex",justifyContent:"space-evenly"}}>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleRegister}>Signup</button>
+      </div>
     </>
   );
 }
---
+++ todo-app/src/components/AllRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllRoutes = () => {
+  return (
+    <>
+      R
+    </>
+  )
+}
+
+export default AllRoutes
