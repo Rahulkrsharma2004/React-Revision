f295ee57191fa5f3abfb81dd34501920395ee1b3
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-02-05 20:12:51 +0530

    done some code

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
    <>
    <h1>WELCOME</h1>
+   <h1>Delete Message here</h1>
+   <del>Here not using anything</del>
+   <mark>This is a highlight</mark>
+   <b>
+     This is bold
+   </b>
    </>
   )
 }
