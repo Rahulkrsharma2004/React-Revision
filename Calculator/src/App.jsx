bedb3405a8eae28b473bc03caf911a2c6cb6eac9
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-03-03 20:17:14 +0530

    datafetch done

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,18 +1,37 @@
-import './App.css'
+import { useState,useEffect } from "react";
+import axios from 'axios'
+const App = () =>{
 
-function App() {
+  const [data,setData] = useState([])
 
-  return (
-   <>
-   <h1>WELCOME</h1>
-   <h1>Delete Message here</h1>
-   <del>Here not using anything</del>
-   <mark>This is a highlight</mark>
-   <b>
-     This is bold
-   </b>
-   </>
+  useEffect(()=>{
+    DataFetch()
+  },[])
+
+  const DataFetch = async() =>{
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      console.log(res.data)
+      setData(res.data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  return(
+    <>
+    <h1>Api Data</h1>
+    {
+      data.map((item)=>(
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>  
+      ))
+    }
+    </>
   )
 }
 
-export default App
+
+export default App;
